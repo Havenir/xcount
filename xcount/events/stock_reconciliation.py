74b5ac4a2b7b0260e239326a1bed0778dc909383
Muhammad Ahmad <mohammad.ahmad.zulfiqar1@gmail.com>
2020-10-10 17:17:12 +0500

    feat: Stock Reconciliation   - make uncounted items as zero

--- xcount/events/stock_reconciliation.py
+++ xcount/events/stock_reconciliation.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import frappe
 import json
+from erpnext.stock.doctype.batch.batch import get_batch_qty, get_batches
 
 
 def validate_items_and_stock_sheets(doc, method):
@@ -14,13 +15,15 @@ def validate_items_and_stock_sheets(doc, method):
             stock_sheet.db_set('reconciled', 1)
 
 @frappe.whitelist()
-def get_items_from_stocksheets(stock_sheets):
+def get_items_from_stocksheets(stock_sheets, warehouse, make_uncounted_items_zero = None):
     #Merge quantity of same items from same warehouse in stock_sheets
     stock_sheets  = json.loads(stock_sheets)
     items = {}
+    items_set = set()
     for row in stock_sheets:
-        stock = frappe.get_doc('Stock Sheet', row['name'])
+        stock = frappe.get_doc('Stock Sheet', row)
         for item in stock.items:
+            items_set.add(item.item_code)
             item_tuple = (item.item_code, item.warehouse, item.batch_no)
             if not item_tuple in items:
                 items[item_tuple] = item.qty
@@ -34,4 +37,30 @@ def get_items_from_stocksheets(stock_sheets):
             'qty': items[row],
             'batch_no': row[2]
         })
+
+    if not item_array: 
+        return item_array
+    
+    if make_uncounted_items_zero:
+        bin_list = frappe.get_list('Bin', filters = {'warehouse': warehouse}, fields = ['item_code'])
+        for row in bin_list:
+            if row.item_code not in items_set:
+
+                has_batch = frappe.get_value('Item', row.item_code, 'has_batch_no')
+                if has_batch: 
+                    batches = get_batches(row.item_code, warehouse)
+                    for batch in batches:
+                        if batch.qty > 0:
+                            item_array.append({
+                                'item_code': row.item_code,
+                                'warehouse': warehouse,
+                                'qty': 0,
+                                'batch_no': batch.batch_id
+                            })
+                else:
+                    item_array.append({
+                    'item_code': row.item_code,
+                    'warehouse': warehouse,
+                    'qty': 0
+                })
     return item_array
--- xcount/public/js/stock_reconciliation.js
+++ xcount/public/js/stock_reconciliation.js
@@ -3,7 +3,6 @@ frappe.ui.form.on('Stock Reconciliation', {
         frm.events.purpose(frm);
     },
     purpose: function (frm) {  
-        console.log(frm.doc.purpose);
         if (frm.doc.purpose === 'Stock Reconciliation'){
             frm.events.create_stock_sheet_button(frm);
         }
@@ -11,50 +10,82 @@ frappe.ui.form.on('Stock Reconciliation', {
     create_stock_sheet_button: function (frm) {  
         if (frm.doc.docstatus === 0){
             frm.add_custom_button(__("Get Stock Count Sheets"), function() {
-                if (!frm.doc.warehouse){
-                    frappe.msgprint('No Warehouse selected')
-                }else{
-                    frm.trigger('get_stock_sheets');
-                }
+                frm.trigger('get_stock_sheets');
 			});
         }
     },
 
     get_stock_sheets: function (frm) {
+        // Dialog to select Stock Sheets
+
+        frappe.prompt({
+            label: 'Warehouse',
+            fieldname: 'warehouse',
+            fieldtype: 'Link',
+            options: 'Warehouse'
+        }, (values) => {
+            new frappe.ui.form.MultiSelectDialog({
+                doctype: "Stock Sheet",
+                target: frm,
+                setters: {
+                    company: frm.doc.company
+                },
+                date_field: "stock_count_date",
+                get_query() {
+                    return {
+                        filters: { 
+                            docstatus: ['=', 1],
+                            default_warehouse: values.warehouse,
+                            reconciled: 0
+                        }
+                    }
+                },
+                action(selections) {
+                    if (selections.length === 0){
+                        frappe.msgprint(`No Stock Count Sheet selected for ${values.warehouse}`);
+                    }
+                    frm.doc.stock_sheets = []
+                    frm.doc.items = []
+                    for (let row in selections) {
+                        frm.add_child('stock_sheets', {
+                            'stock_sheet': selections[row].name,
+                            'stock_count_date': selections[row].stock_count_date
+                        })
+                    }
+        
+                    frm.refresh_field('stock_sheets');
+        
+                    frappe.call('xcount.events.stock_reconciliation.get_items_from_stocksheets', 
+                    {
+                        stock_sheets : selections, 
+                        warehouse: values.warehouse, 
+                        make_uncounted_items_zero: frm.doc.make_uncounted_items_zero
+                    })
+                    .then(r => {
+                        for (let row in r.message) {
+                            frm.add_child('items', {
+                                item_code: r.message[row].item_code,
+                                warehouse: r.message[row].warehouse,
+                                qty: r.message[row].qty,
+                                batch_no: r.message[row].batch_no
+                            })
+                        }
+                        frm.refresh_field('items');
+                        cur_dialog.hide();
+                    })
+                }
+            });
+        })
+        
+
         frappe.db.get_list('Stock Sheet', {
             fields: ['name', 'stock_count_date', 'default_warehouse'],
             filters: {
                 reconciled: 0,
                 docstatus: 1
             }
-        }).then(records => {
-            if (records.length === 0){
-                frappe.msgprint(`No Stock Count Sheet pending for ${frm.doc.warehouse}`);
-            }
-            frm.doc.stock_sheets = []
-            frm.doc.items = []
-            for (let row in records) {
-                frm.add_child('stock_sheets', {
-                    'stock_sheet': records[row].name,
-                    'stock_count_date': records[row].stock_count_date
-                })
-            }
-
-            frm.refresh_field('stock_sheets');
-
-            frappe.call('xcount.events.stock_reconciliation.get_items_from_stocksheets', 
-            {stock_sheets : records})
-            .then(r => {
-                for (let row in r.message) {
-                    frm.add_child('items', {
-                        'item_code': r.message[row].item_code,
-                        'warehouse': r.message[row].warehouse,
-                        'qty': r.message[row].qty,
-                        'batch_no': r.message[row].batch_no
-                    })
-                }
-                frm.refresh_field('items');
-            })
+        }).then(selections => {
+            
         })
         
     },
